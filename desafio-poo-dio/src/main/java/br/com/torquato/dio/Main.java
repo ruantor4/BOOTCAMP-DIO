baaacb2677291407381fe97e724073e2c8048f0b
Ruan Torquato <ruan.tor4@hotmail.com>
2025-01-22 21:30:04 -0300

    Curso e Mentoria

--- desafio-poo-dio/src/main/java/br/com/torquato/dio/Main.java
+++ desafio-poo-dio/src/main/java/br/com/torquato/dio/Main.java
@@ -1,7 +1,29 @@
 package br.com.torquato.dio;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        curso.setTitulo("Curso JAVA");
+        curso.setDescricao("Descrição curso JAVA");
+        curso.setCargaHoraria(8);
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso JAVA2");
+        curso1.setDescricao("Descrição curso JAVA2");
+        curso1.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria JAVA");
+        mentoria.setDescricao("Descrição mentoria JAVA");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso1);
+        System.out.println(mentoria);
+
+
     }
+    
 }
